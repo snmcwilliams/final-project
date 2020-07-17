5f06df0bd36c80f26e8c02f0460bfc14b7fc3b8d
Sarah McWilliams <snmcwilliams@gmail.com>
2020-07-17 19:17:05 -0400

    Added modal

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Slideshow
+
 var slideIndex = 0;
 showSlides();
 
@@ -11,4 +13,23 @@ function showSlides() {
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 9000); // Change image every 9 seconds
+}
+
+// Modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal
+var img = document.getElementById("myImg1");
+var modalImg = document.getElementById("img01");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
 }
